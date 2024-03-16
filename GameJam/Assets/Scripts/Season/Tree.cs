efbda690d14b763e1003188ed5c9b1c5df07a43f
andyDPY <a.dupouy@lahorde.school>
2024-03-16 16:34:25 +0100

    crotte

--- GameJam/Assets/Scripts/Season/Tree.cs
+++ GameJam/Assets/Scripts/Season/Tree.cs
@@ -8,24 +8,35 @@ public class Tree : SeasonObject
     public Sprite treeSpriteWithLeaves;
     public Sprite treeSpriteWithoutLeaves;
 
+    public GameObject leaf;
+
+    private void Awake()
+    {
+        leaf.SetActive(false);
+    }
+
     public override void SeasonChanged(string season)
     {
         switch(season)
         {
             case "summer":
                 treeSprite.sprite = treeSpriteWithLeaves;
+                leaf.SetActive(false);
                 break;
 
             case "winter":
                 treeSprite.sprite = treeSpriteWithoutLeaves;
+                leaf.SetActive(false);
                 break;
             
             case "fall":
                 treeSprite.sprite = treeSpriteWithoutLeaves;
+                leaf.SetActive(true);
                 break;
                 
             case "spring":
                 treeSprite.sprite = treeSpriteWithLeaves;
+                leaf.SetActive(false);
                 break;  
         }
     }
