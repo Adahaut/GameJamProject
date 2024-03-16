98f2d913feafbb8aa983b2c35b2a167301383216
Clayams <118185670+Clayams@users.noreply.github.com>
2024-03-16 09:19:19 +0100

    Controller menu

--- GameJam/Assets/Scripts/Menu/Menu_Navigation.cs
+++ GameJam/Assets/Scripts/Menu/Menu_Navigation.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu_Navigation : MonoBehaviour
 {
     [SerializeField] GameObject mainMenu;
     [SerializeField] GameObject settingsMenu;
     [SerializeField] GameObject creditsMenu;
+
+    [SerializeField] GameObject playbtn;
+    [SerializeField] GameObject backSettings;
+    [SerializeField] GameObject backCredits;
+
+
         
     public void Start()
     {
@@ -23,7 +31,7 @@ public class Menu_Navigation : MonoBehaviour
 
     public void SettingsButton()
     {
-        Debug.Log("bite;");
+        EventSystem.current.SetSelectedGameObject(backSettings);
         settingsMenu.SetActive(true);
         mainMenu.SetActive(false);
         creditsMenu.SetActive(false);
@@ -31,6 +39,7 @@ public class Menu_Navigation : MonoBehaviour
 
     public void CreditsButton()
     {
+        EventSystem.current.SetSelectedGameObject(backCredits);
         creditsMenu.SetActive(true);
         mainMenu.SetActive(false);
         settingsMenu.SetActive(false);
@@ -38,6 +47,7 @@ public class Menu_Navigation : MonoBehaviour
 
     public void BackButtonForMenu()
     {
+        EventSystem.current.SetSelectedGameObject(playbtn);
         mainMenu.SetActive(true);
         settingsMenu.SetActive(false);
         creditsMenu.SetActive(false);
